d653bea13ed0c703cfbbd27e038e1ab41272d6c1
DoyoungLee9720 <dlehdud224@gmail.com>
2024-11-20 16:21:04 +0900

    start

---
+++ src/main/java/com/lotte2backteam1/Lotte2ndProjectBackTeam1Application.java
@@ -0,0 +1,13 @@
+package com.lotte2backteam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lotte2ndProjectBackTeam1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lotte2ndProjectBackTeam1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lotte2backteam1/Lotte2ndProjectBackTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotte2backteam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lotte2ndProjectBackTeam1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
