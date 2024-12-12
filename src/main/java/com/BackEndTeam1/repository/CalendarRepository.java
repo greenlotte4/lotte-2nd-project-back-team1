32b41b030f17d594a4cce1ae9b29dce72fa5ebb0
DoyoungLee9720 <dlehdud224@gmail.com>
2024-12-12 17:32:17 +0900

    달력 이벤트 추가기능

--- src/main/java/com/BackEndTeam1/repository/CalendarRepository.java
+++ src/main/java/com/BackEndTeam1/repository/CalendarRepository.java
@@ -3,6 +3,10 @@ package com.BackEndTeam1.repository;
 import com.BackEndTeam1.entity.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CalendarRepository extends JpaRepository<Calendar, Integer> {
     boolean existsByCalendarCode(String inviteCode);
+
+    Optional<Object> findByCalendarCode(String calendarCode);
 }
